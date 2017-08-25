eeeb8e66943e274cf1bde128b6a9237215e7d811
vsko <info@zoom.net.ua>
2017-08-25 16:37:17 +0300

    Fixed postfix RedbeanExtension to RedbeanLibrary

--- library.php
+++ library.php
@@ -1,6 +1,6 @@
 <?php
 
-class RedbeanExtension extends Sumish\Library {
+class RedbeanLibrary extends Sumish\Library {
     public function init() {
         $this->load(__DIR__ . '/rb.php');
         return true;
